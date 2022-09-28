9fa063f0b442f6722376cf1f0048f3ffd113fd81
Srikanthnaid <ksru044gmail.com>
2022-09-28 08:41:36 +0530

    Create a addressBook

---
+++ com/bridgelab/Contact.java
@@ -0,0 +1,98 @@
+package com.bridgelab;
+
+public class Contact {
+	private String firstName, lastName, address, city, state, email;
+	private int zip;
+	private long phoneNumber;
+
+	/**
+	 * Used Constructor to store the Parameter
+	 * 
+	 * @param firstName
+	 * @param lastName
+	 * @param address
+	 * @param city
+	 * @param state
+	 * @param email
+	 * @param zip
+	 * @param phoneNumber
+	 */
+	public Contact(String firstName, String lastName, String address, String city, String state, String email, int zip,
+			long phoneNumber) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.email = email;
+		this.zip = zip;
+		this.phoneNumber = phoneNumber;
+	}
+
+	/**
+	 * Used getter and setter to set and get the value.
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+}
