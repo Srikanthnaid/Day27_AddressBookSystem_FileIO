ad99dbb47c0bd5b2b2bc48cf89cf441fe0d67e05
Srikanthnaid <ksru044gmail.com>
2022-09-28 08:48:27 +0530

    Add new contact list in addressbook

--- com/bridgelab/AddressBookSystem.java
+++ com/bridgelab/AddressBookSystem.java
@@ -1,30 +1,79 @@
 package com.bridgelab;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBookSystem {
 	/**
-     * create a main method ,all program execute in main method
+     * create a array list object
+     * The ArrayList class of the Java collections framework provides the functionality of resizable-arrays.
+     * It implements the List interface.
+     */
+    ArrayList<Contact> arrayDetails = new ArrayList<Contact>();
+    /**
+     * The Scanner class is used to get user input, and it is found in the java.util package.
+     * create a scanner class object
+     */
+	Scanner sc = new Scanner(System.in);
+
+	/**
+	 * This method is used to add details to address book
+	 */
+    public void addDetails() {
+        /**
+         * create a object an contacts class.
+         * object name is info
+         */
+        Contact info = new Contact();
+
+        System.out.println("Enter the first name");
+        info.setFirstName(sc.nextLine());
+        System.out.println("Enter the last name");
+        info.setLastName(sc.nextLine());
+        System.out.println("Enter the address");
+        info.setAddress(sc.nextLine());
+        System.out.println("Enter the city");
+        info.setCity(sc.nextLine());
+        System.out.println("Enter the state");
+        info.setState(sc.nextLine());
+        System.out.println("Enter the email");
+        info.setEmail(sc.nextLine());
+        System.out.println("Enter the zip code");
+        info.setZip(sc.nextInt());
+        System.out.println("Enter the phone number");
+        info.setPhoneNumber(sc.nextLong());
+        arrayDetails.add(info);
+        sc.close();
+    }
+
+    /**
+     * This method is used to display the added information
+     */
+    public void display() {
+        System.out.println(arrayDetails);
+    }
+
+    /**
+     * create a main method all program execute in main method
      * @param args no arguments
      */
-
     public static void main(String[] args) {
         /**
-         * 1st print welcome msg.
+         * 1st print the welcome msg.
          */
-        System.out.println("Welcome to Address Book System\n");
+        System.out.println("Welcome to Address Book Program");
         /**
-         * We are just calling an object using class name.
-         * Simply passing the argument value according to the parameter defined in the constructor.
+         * create a object name as details for  AddressBookSystem  class
+         * create object bcoz AddressBookSystem in this class all method is non static
          */
-        Contact details = new Contact("NSrikanth", "Naidu", "Alampur", "Hyderabad",
-                "Telangana", "nksru044@gmail.com", 4123, 630166309);
-        System.out.println("The following contact details is mentioned below : \n");
-        System.out.println("First Name : " + details.getFirstName());
-        System.out.println("Last Name  : " + details.getLastName());
-        System.out.println("Address    : " + details.getAddress());
-        System.out.println("City       : " + details.getCity());
-        System.out.println("State      : " + details.getState());
-        System.out.println("E-mail     : " + details.getEmail());
-        System.out.println("Zip Code   : " + details.getZip());
-        System.out.println("Phone No   : " + details.getPhoneNumber());
+        AddressBookSystem details = new AddressBookSystem();
+        /**
+         * calling method to object name . method name
+         * object name.method name;
+         * object=details
+         * method=addDetails(),display()
+         */
+        details.addDetails();
+        details.display();
     }
 }
